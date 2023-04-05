0e86820a92049145d1e94be0bc59ffe708b8416f
Sakherr <sakhershteyat@gmail.com>
2023-04-05 21:52:25 +0000

    SS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const dbSchemaPath = path.join(__dirname, 'schema.sql');
 const dbSchema = fs.readFileSync(dbSchemaPath).toString();
